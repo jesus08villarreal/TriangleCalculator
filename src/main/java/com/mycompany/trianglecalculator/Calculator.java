42bd6d72eeabe0689e1c925965c03097272b88d6
jesusvs1100 <jesusvs1100@gmail.com>
2024-06-19 20:22:21 -0600

    first and final commit

---
+++ src/main/java/com/mycompany/trianglecalculator/Calculator.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trianglecalculator;
+
+/**
+ *
+ * @author jesus
+ */
+public class Calculator {
+    private double base;
+    private double altura;
+
+    public Calculator(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
+
+    public double calcularPerimetro() {
+        return 3 * base;
+    }
+}
